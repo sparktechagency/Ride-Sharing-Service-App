7605c89103086f23527c000b9dd5def168598b8c
mrswapon <mrswapon15@gmail.com>
2025-04-26 11:47:48 +0600

    Driver Home Screen Done

--- lib/views/screen/Notifications/notifications_screen.dart
+++ lib/views/screen/Notifications/notifications_screen.dart
@@ -18,90 +18,89 @@ class NotificationsScreen extends StatelessWidget {
       appBar: CustomAppBar(title: AppStrings.notification.tr),
       //================================> Body section <=======================
       body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.h),
-          child: Column(
-            children: [
-              //================================> Notification section <=======================
-              Expanded(
-                child: ListView.builder(
-                  itemCount: 6,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: EdgeInsets.only(
-                          bottom: 16.h, top: index == 0 ? 16.h : 0),
-                      child: _Notification(
-                        'Ariana your view this',
-                        DateTime.now().subtract(Duration(minutes: index * 10)),
-                      ),
-                    );
-                  },
-                ),
+        padding: EdgeInsets.symmetric(horizontal: 20.h),
+        child: Column(
+          children: [
+            //================================> Notification section <=======================
+            Expanded(
+              child: ListView.builder(
+                itemCount: 6,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      bottom: 16.h,
+                      top: index == 0 ? 16.h : 0,
+                    ),
+                    child: _Notification(
+                      'Ariana your view this',
+                      DateTime.now().subtract(Duration(minutes: index * 10)),
+                    ),
+                  );
+                },
               ),
-            ],
-          ),
-
-      )
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
-  _Notification(
-      String title,
-      DateTime time,
-      ) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 8.w),
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(4.r)),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Row(
+_Notification(String title, DateTime time) {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 8.w),
+    decoration: BoxDecoration(borderRadius: BorderRadius.circular(4.r)),
+    child: Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                //==============================> Notification Icon <=========================
+                Container(
+                  margin: EdgeInsets.only(right: 8.w),
+                  padding: EdgeInsets.all(8.w),
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xffffffff),
+                  ),
+                  child: SvgPicture.asset(
+                    AppIcons.notification,
+                    width: 32.w,
+                    height: 32.h,
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  //==============================> Notification Icon <=========================
-                  Container(
-                    margin: EdgeInsets.only(right: 8.w),
-                    padding: EdgeInsets.all(8.w),
-                    decoration: const BoxDecoration(
-                        shape: BoxShape.circle, color: Color(0xffffffff)),
-                    child: SvgPicture.asset(AppIcons.notification,
-                        width: 32.w,
-                        height: 32.h,
-                        color: AppColors.primaryColor),
-                  )
+                  //==============================> Notification Text <=========================
+                  CustomText(
+                    text: title,
+                    fontSize: 14.h,
+                    maxLine: 3,
+                    textAlign: TextAlign.start,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: CustomText(
+                      top: 2.h,
+                      text: TimeAgo.format(time),
+                      fontWeight: FontWeight.w400,
+                      color: const Color(0xFF8C8C8C),
+                    ),
+                  ),
                 ],
               ),
-              Expanded(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    //==============================> Notification Text <=========================
-                    CustomText(
-                      text: title,
-                      fontSize: 14.h,
-                      maxLine: 3,
-                      textAlign: TextAlign.start,
-                    ),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: CustomText(
-                        top: 2.h,
-                        text: TimeAgo.format(time),
-                        fontWeight: FontWeight.w400,
-                        color: const Color(
-                          0xFF8C8C8C,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Divider(color: AppColors.textColor),
-        ],
-      ),
-    );
-  }
+            ),
+          ],
+        ),
+        Divider(color: AppColors.textColor),
+      ],
+    ),
+  );
+}
--- lib/views/screen/Role/Driver/Home/driver_home_screen.dart
+++ lib/views/screen/Role/Driver/Home/driver_home_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:ride_sharing/utils/app_colors.dart';
 import 'package:ride_sharing/utils/app_strings.dart';
+import 'package:ride_sharing/views/base/custom_network_image.dart';
 import 'package:ride_sharing/views/base/custom_text.dart';
 import '../../../../../helpers/route.dart';
 import '../../../../../utils/app_icons.dart';
@@ -24,7 +25,7 @@ class DriverHomeScreen extends StatelessWidget {
             Spacer(),
             InkWell(
               onTap: () {
-                //Get.toNamed(AppRoutes.notificationsScreen);
+                Get.toNamed(AppRoutes.notificationsScreen);
               },
               child: SvgPicture.asset(AppIcons.notification),
             ),
@@ -37,7 +38,7 @@ class DriverHomeScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(
-              height: 171.h,
+              height: 191.h,
               child: ListView.builder(
                 shrinkWrap: true,
                 scrollDirection: Axis.horizontal,
@@ -53,26 +54,33 @@ class DriverHomeScreen extends StatelessWidget {
                           color: AppColors.borderColor,
                         ),
                       ),
-                      child: Padding(
-                        padding: EdgeInsets.all(16.w),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            SvgPicture.asset(AppIcons.orderTR),
-                            SizedBox(height: 12.h),
-                            CustomText(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 12.h,
+                              vertical: 16.h,
+                            ),
+                            child: SvgPicture.asset(AppIcons.orderTR),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 12.h),
+                            child: CustomText(
                               text: AppStrings.recentOrder.tr,
                               fontWeight: FontWeight.w500,
                               fontSize: 18.sp,
                             ),
-                            SizedBox(height: 12.h),
-                            CustomText(
-                              text: 'Total(12)'.tr,
-                              fontWeight: FontWeight.w500,
-                              fontSize: 18.sp,
-                            ),
-                          ],
-                        ),
+                          ),
+                          SizedBox(height: 6.h),
+                          Divider(thickness: 1.5, color: AppColors.borderColor),
+                          SizedBox(height: 6.h),
+                          CustomText(
+                            text: 'Total(12)'.tr,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 18.sp,
+                          ),
+                        ],
                       ),
                     ),
                   );
@@ -80,11 +88,150 @@ class DriverHomeScreen extends StatelessWidget {
               ),
             ),
             SizedBox(height: 24.h),
+            //================================> Recently accepted order <=======================
             CustomText(
               text: AppStrings.recentlyAcceptedOrder.tr,
               fontSize: 20.sp,
               fontWeight: FontWeight.w500,
-            )
+              bottom: 16.h,
+            ),
+            Expanded(
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: 3,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(bottom: 8.h),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16.r),
+                        border: Border.all(
+                          width: 1.w,
+                          color: AppColors.borderColor,
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          //========================> Top Container <=================
+                          Padding(
+                            padding: EdgeInsets.all(10.w),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    CustomNetworkImage(
+                                      imageUrl:
+                                          'https://t4.ftcdn.net/jpg/02/24/86/95/360_F_224869519_aRaeLneqALfPNBzg0xxMZXghtvBXkfIA.jpg',
+                                      height: 38.h,
+                                      width: 38.w,
+                                      boxShape: BoxShape.circle,
+                                    ),
+                                    SizedBox(width: 8.w),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        CustomText(
+                                          text: 'Mr. Imran',
+                                          bottom: 4.h,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                        Row(
+                                          children: [
+                                            CustomText(text: '4.9', right: 4.w),
+                                            SvgPicture.asset(AppIcons.star),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                //========================> Status Container <=================
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color: AppColors.primaryColor,
+                                    borderRadius: BorderRadius.circular(10.r),
+                                  ),
+                                  child: Padding(
+                                    padding: EdgeInsets.symmetric(
+                                      horizontal: 12.h,
+                                      vertical: 4.h,
+                                    ),
+                                    child: CustomText(
+                                      text: AppStrings.ongoing,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Divider(thickness: 1.5, color: AppColors.borderColor),
+                          //========================> Details Container <=================
+                          Padding(
+                            padding: EdgeInsets.all(12.w),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                CustomText(
+                                  text: '\$15.99',
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 22.sp,
+                                  bottom: 8.h,
+                                ),
+                                CustomText(
+                                  text:
+                                      'Booking Time : Sat 12 April 2025  8.30 PM',
+                                  fontWeight: FontWeight.w500,
+                                  bottom: 8.h,
+                                ),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        CustomText(
+                                          text: AppStrings.pICKUP,
+                                          right: 4.w,
+                                          bottom: 12.h,
+                                        ),
+                                        CustomText(text: 'Dhaka', right: 4.w),
+                                      ],
+                                    ),
+                                    SizedBox(
+                                      width: 102.w,
+                                      child: Divider(
+                                        thickness: 1.5,
+                                        color: AppColors.borderColor,
+                                      ),
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        CustomText(
+                                          text: AppStrings.dROPOFF,
+                                          left: 4.w,
+                                          bottom: 12.h,
+                                        ),
+                                        CustomText(text: 'Rangpur', left: 4.w),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),

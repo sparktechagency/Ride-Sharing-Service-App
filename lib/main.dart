db2b3a26835d64f98bf118f6fd08bc381a2c3da6
mrswapon <mrswapon15@gmail.com>
2025-05-23 15:42:23 +0600

    Ride Details Screen Done

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class MyApp extends StatelessWidget {
                   ),
                   transitionDuration: const Duration(milliseconds: 500),
                   getPages: AppRoutes.page,
-                  initialRoute: AppRoutes.userSearchScreen,
+                  initialRoute: AppRoutes.splashScreen,
                 );
               },
             );

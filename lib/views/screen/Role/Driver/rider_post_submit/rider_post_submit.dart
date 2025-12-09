96f3305979481df71a24ca6a4312228d0c76d828
eastodev288 <infoeato288@gmail.com>
2025-12-09 19:20:01 +0600

    fix  add city feature

--- lib/views/screen/Role/Driver/rider_post_submit/rider_post_submit.dart
+++ lib/views/screen/Role/Driver/rider_post_submit/rider_post_submit.dart
@@ -1,45 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get_navigation/src/extension_navigation.dart';
-import 'package:get/utils.dart';
+import 'package:get/get.dart';
 import 'package:ride_sharing/utils/app_colors.dart';
 import 'package:ride_sharing/utils/app_images.dart';
 import 'package:ride_sharing/utils/app_strings.dart';
 import 'package:ride_sharing/views/base/custom_button.dart';
-
 class RiderPostSubmit extends StatelessWidget {
   const RiderPostSubmit({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      appBar:  AppBar(
-        backgroundColor: AppColors.primaryColor,
+      backgroundColor: AppColors.whiteColor,
+      appBar: AppBar(
+        backgroundColor: AppColors.whiteColor,
         elevation: 0,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios_new_rounded, color: Colors.white),
+          icon: Icon(Icons.arrow_back_ios, color: AppColors.blackColor),
           onPressed: () => Get.back(),
         ),
-        title: Text(
-          AppStrings.createRide.tr,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 18.sp,
-            fontWeight: FontWeight.w600,
-          ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 32.w),
+        child: Column(
+          children: [
+            const Spacer(),
+            Container(
+              height: 264.h,
+              width: double.infinity,
+              child: Image.asset(AppImages.riderPost, fit: BoxFit.contain),
+            ),
+            SizedBox(height: 48.h),
+            // Title Text
+            Text(
+              AppStrings.riderPostSubmit.tr,
+              style: TextStyle(
+                fontSize: 28.sp,
+                fontWeight: FontWeight.bold,
+                color: AppColors.blackColor,
+                height: 1.2,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 48.h),
+            CustomButton(onTap: () {}, text: AppStrings.postRides.tr),
+            const Spacer(),
+          ],
         ),
       ),
-      body: Column(
-        children: [
-          Image.asset(AppImages.riderPost,width: 100.w,height: 100.h,),
-          Spacer(),
-          Text(AppStrings.riderPostSubmit.tr,style: TextStyle(color: AppColors.blackColor,fontSize: 16.sp),),
-          SizedBox(height: 12.h,),
-          CustomButton(onTap: (){}, text: AppStrings.submit.tr)
-        ],
-      ),
-
     );
   }
 }

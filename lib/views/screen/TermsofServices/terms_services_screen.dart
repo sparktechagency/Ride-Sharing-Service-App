c5759ca9a78dfae4f0907a40c9b8f518e4507387
Md Nurujjaman <mdnurujjaman329@gmail.com>
2025-12-26 17:20:16 +0600

    show terms and condition and policy page

--- lib/views/screen/TermsofServices/terms_services_screen.dart
+++ lib/views/screen/TermsofServices/terms_services_screen.dart
@@ -1,42 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-
+import '../../../controllers/privacy_terms_controller.dart';
 import '../../../utils/app_strings.dart';
 import '../../base/custom_app_bar.dart';
 import '../../base/custom_text.dart';
 
-class TermsServicesScreen extends StatelessWidget {
+class TermsServicesScreen extends StatefulWidget {
   const TermsServicesScreen({super.key});
+
+  @override
+  State<TermsServicesScreen> createState() => _TermsServicesScreenState();
+}
+
+class _TermsServicesScreenState extends State<TermsServicesScreen> {
+  final PrivacyTermsController termsController = Get.isRegistered<PrivacyTermsController>()
+      ? Get.find<PrivacyTermsController>()
+      : Get.put(PrivacyTermsController(), permanent: true);
+
+  @override
+  void initState() {
+    super.initState();
+    termsController.fetchTermsConditions();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: AppStrings.termsConditions.tr),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 16.h),
-          child: Column(
-            children: [
-              CustomText(
-                text:
-                    'Lorem ipsum dolor sit amet consectetur. Enim sagittis mattis sed risus nunc. Non ac vel viverra ut facilisis ultricies leo. Vel nunc eget tellus duis mollis sollicitudin. Eget aliquam leo sed arcu. Dignissim enim dolor rhoncus nam nisi ullamcorper sed suscipit pellentesque. Volutpat magna imperdiet cum adipiscing quam curabitur consectetur. At tortor consectetur ut ut scelerisque nec elementum tellus. Consectetur quis amet duis quisque suspendisse. Pellentesque scelerisque venenatis blandit velit ac eu.',
-                fontSize: 14.sp,
-                maxLine: 20,
-                textAlign: TextAlign.start,
-                bottom: 24.h,
-              ),
-              CustomText(
-                text:
-                    'Lorem ipsum dolor sit amet consectetur. Enim sagittis mattis sed risus nunc. Non ac vel viverra ut facilisis ultricies leo. Vel nunc eget tellus duis mollis sollicitudin. Eget aliquam leo sed arcu. Dignissim enim dolor rhoncus nam nisi ullamcorper sed suscipit pellentesque. Volutpat magna imperdiet cum adipiscing quam curabitur consectetur. At tortor consectetur ut ut scelerisque nec elementum tellus. Consectetur quis amet duis quisque suspendisse. Pellentesque scelerisque venenatis blandit velit ac eu.',
-                fontSize: 14.sp,
-                maxLine: 20,
-                textAlign: TextAlign.start,
-              ),
-            ],
+      body: Obx(() {
+        if (termsController.isLoadingTermsConditions.value) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        final content = termsController.getTermsConditionsContent();
+
+        if (content.isEmpty) {
+          return  Center(
+            child: CustomText(
+              text: 'No terms and conditions content available',
+              fontSize: 16.sp,
+            ),
+          );
+        }
+
+        return SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 16.h),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomText(
+                  text: content,
+                  fontSize: 14.sp,
+                  textAlign: TextAlign.start,
+                  // 1. Set maxLine to null to allow infinite expansion
+                  maxLine: null,
+                  // 2. Set textOverflow to visible so it doesn't add "..."
+                  textOverflow: TextOverflow.visible,
+                ),
+              ],
+            ),
           ),
-        ),
-      ),
+        );
+      }),
     );
   }
 }
